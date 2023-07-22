f52b2340c3f5185d48a7ecd4e45c5431665b12c9
davidepalleschi1216 <140216125+davidepalleschi1216@users.noreply.github.com>
2023-07-22 02:09:13 +0200

    Upload

---
+++ Home.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="manifest" href="manifest.json" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=s, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="apple-touch-icon" href="Img/logo.png" />
+    <meta name="apple-mobile-web-app-status-bar" content="#2B2A2A" />
+    <meta name="theme-color" content="#2B2A2A" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body style="background-color: #2B2A2A; margin: 0;">
+
+    <div class="topnav">
+        <a class="Title" href="Home.php">Orate Fratres</a>
+        <a class="Settings" href="Login.php"><ion-icon name="person-circle-outline"></ion-icon></a>
+    </div>
+
+
+    <div style="padding-inline: 13px;">
+        <p style="font-weight: bold;"> Santo del giorno:</p>
+        <div style="background-color: #4C4E4D; padding: 1px 10px 1px 10px;  border-radius: 20px;">
+            <h2 style="text-align: center;">Beata Vergine Maria del Monte Carmelo</h2>
+            <p>
+                Beata Maria Vergine del Monte Carmelo, dove un tempo il profeta Elia aveva ricondotto il popolo di Israele al culto del Dio vivente e si ritirarono poi degli eremiti in cerca di solitudine, istituendo un Ordine di vita contemplativa sotto il patrocinio della santa Madre di Dio.
+            </p>
+        </div>
+    </div>
+    <p style="padding-left: 17px; font-weight: bold;">Preghiere del Cristiano:</p>
+    <div class="Row">
+        <table>
+            <tr>
+                <td style="padding-left: 5px; "><a href="">
+                        <div>Segno della Croce</div>
+                    </a></td>
+                <td><a href="">
+                        <div>Padre Nostro</div>
+                    </a></td>
+                <td><a href="">
+                        <div>Gloria al Padre</div>
+                    </a></td>
+                <td><a href="">
+                        <div>Ave Maria</div>
+                    </a></td>
+                <td><a href="">
+                        <div>Credo Apostolico</div>
+                    </a></td>
+            </tr>
+        </table>
+    </div>
+
+    <p style="padding-left: 17px; font-weight: bold;">Novene:</p>
+    <div class="Novene">
+        <table>
+            <tr>
+                <td style="padding-left: 10px; "><a href="">
+                        <div style="width: 150px; height: 120px;">
+                            <img src="img/SanTommaso.png" alt="" class="img_Novena">
+                            <p class="nome_Novena">Novena della Milizia Angelica</p>
+                        </div>
+                    </a></td>
+                <td><a href="">
+                        <div style="width: 150px; height: 120px;">
+                            <img src="img/Natale.png" alt="" class="img_Novena">
+                            <p class="nome_Novena">Novena del Santo Natale</p>
+                        </div>
+                    </a></td>
+                <td style="padding-right: 10px; "><a href="">
+                        <div style="width: 150px; height: 120px;">
+                            <img src="img/MadonnaDelCarmelo.png" alt="" class="img_Novena">
+                            <p class="nome_Novena">Novena alla Madonna del Carmelo</p>
+                        </div>
+                    </a></td>
+            </tr>
+        </table>
+    </div>
+
+    <p style="padding-left: 17px; font-weight: bold;">Piccolo Compendio Catechistico:</p>
+    <div class="CompendioCatechistico">
+  <table>
+    <tr>
+      <td style="padding-left: 10px; ">
+        <a href="">
+          <div>
+            <p>I Sette Doni dello Spirito Santo</p>
+          </div>
+        </a>
+      </td>
+      <td>
+        <a href="">
+          <div>
+            <p> I Sette<br>Sacramenti </p>
+          </div>
+        </a>
+      </td>
+      <td style="padding-right: 10px; ">
+        <a href="">
+          <div>
+            <p>I Sette Doni dello Spirito Santo</p>
+          </div>
+        </a>
+      </td>
+    </tr>
+  </table>
+</div>
+
+
+
+
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+
+</html>
---
+++ Login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="manifest" href="manifest.json" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=s, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+    
+    <link rel="apple-touch-icon" href="Images/Icons/58.png" />
+    <link rel="apple-touch-icon" href="Images/Icons/60.png" />
+    <link rel="apple-touch-icon" href="Images/Icons/80.png" />
+    <link rel="apple-touch-icon" href="Images/Icons/87.png" />
+    <link rel="apple-touch-icon" href="Images/Icons/114.png" />
+    <meta name="apple-mobile-web-app-status-bar" content="#2B2A2A" />
+    <meta name="theme-color" content="#2B2A2A" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+   
+</head>
+<body style="background-color: #2B2A2A; margin: 0;">
+
+    <div class="topnav">
+
+        <a class="Back" href="Home.php"><ion-icon name="arrow-back-outline"></ion-icon></a>
+    </div>
+
+    <center>
+       
+    </center>
+
+    <div  style="padding-left: 17px; padding-right: 17px;">
+        <h3>Login:</h3>
+        <form action="" method="post">
+            <center>
+            <input class="Input_text" type="email" name="Email" placeholder="Email:"><br><br>
+            <input class="Input_text" type="Password" name="Password" placeholder="Password:"><br><br>
+            <input class="Submit" type="submit" value="Login">
+            </center>
+        </form>
+        
+    </div>
+
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
